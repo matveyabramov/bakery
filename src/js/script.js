ac7fcebef1962cb48b7ce51c6e1c58564042d0ce
Matvey <matveyabramv@gmail.com>
2023-08-13 13:20:50 +0200

    Made and adapded Reviews section, added Swiper library, minor changes

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,44 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const nav = document.querySelector('.nav_header'),
-        navItems = document.querySelectorAll('.nav__item'),
-        hamburger = document.querySelector('.hamburger')
 
+    //Hamburger
+
+    //Variables
+    const nav = document.querySelector('.nav_header')
+    const navItems = document.querySelectorAll('.nav__item')
+    const hamburger = document.querySelector('.hamburger')
+
+    //Click on the hamburger
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active')
         nav.classList.toggle('nav_active')
     })
 
+    //Click on the link
     navItems.forEach(item => {
         item.addEventListener('click', () => {
             hamburger.classList.toggle('hamburger_active')
             nav.classList.toggle('nav_active')
         })
+    })
+
+    //Swiper
+
+    new Swiper('.swiper', {
+        loop: true,
+        speed: 800,
+
+        navigation: {
+            nextEl: '.reviews__button_next',
+            prevEl: '.reviews__button_prev'
+        },
+
+        pagination: {
+            el: '.reviews__dots',
+            clickable: true
+        },
+
+        autoplay: {
+            delay: 3000
+        }
     })
 })
